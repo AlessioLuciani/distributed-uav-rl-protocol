e5ab1ec7e89ec07d4ad7b98cee1cdce9338ec060
AlessioLuciani <alessio99.luciani@gmail.com>
2021-02-27 16:48:07 +0100

    Fix conflicts

--- simulation.py
+++ simulation.py
@@ -21,13 +21,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
+
+
 class Simulator(QWidget):
 
     def __init__(self, screen_w, screen_h):
         super().__init__()
         self.w_scale = int((screen_w // self.width) * 0.75)
         self.h_scale = int((screen_h // self.height) * 0.75)
-
+        self.cycle_num = 0
         self.init_window()
 
     @property
@@ -44,17 +46,17 @@ class Simulator(QWidget):
 
     @property
     def avgAoI(self):
-        return np.mean(aois)
+        return np.mean(options.aois)
 
     @property
     def peakAoI(self):
-        return np.max(aois)
+        return np.max(options.aois)
 
     def get_trajectory(self, drone_index):
-        return chosen_locations[drone_index]
+        return options.chosen_locations[drone_index]
 
     def updateCurrentCyle(self):
-        self.tableWidget.item(3, 1).setText("{}/{}".format(self.currentCycle, cycles_num))
+        self.tableWidget.item(3, 1).setText("{}/{}".format(self.currentCycle, options.cycles_num))
 
     def updateAvgAoI(self):
         self.tableWidget.item(4, 1).setText(str(self.avgAoI))
@@ -116,7 +118,7 @@ class Simulator(QWidget):
                            int(center_y - bs_size / 2),
                            bs_size, bs_size, broadcast_pixmap)
 
-        for i, (x, y) in enumerate(drones_locations):
+        for i, (x, y) in enumerate(options.drones_locations):
             target_x, target_y = options.sensing_locations[self.get_trajectory(i)]
 
             painter.drawPixmap(int(center_x + x * self.w_scale),
@@ -158,33 +160,41 @@ def get_screen_resolution(app):
 
 
 def get_location_aoi(cycle, location_index):
-    return (cycle * cycle_length) - ((aois[location_index] // cycle_length) * cycle_length)
+    return (cycle * options.cycle_length) - ((options.aois[location_index] // options.cycle_length) * options.cycle_length)
 
 def get_accumulated_aoi(cycle):
     return max([sum([get_location_aoi(cycle, location) for location in range(options.sensing_locations_amount)]), 0.0001])
 
 def get_trajectory(drone_index):
-    chosen_location_index = chosen_locations[drone_index]
-    chosen_location = sensing_locations[chosen_location_index]
-    drone_location = drones_locations[drone_index]
+    chosen_location_index = options.chosen_locations[drone_index]
+    chosen_location = options.sensing_locations[chosen_location_index]
+    drone_location = options.drones_locations[drone_index]
     distance = np.linalg.norm(chosen_location - drone_location)
-    if distance <= options.drone_max_speed * cycle_length:
+    if distance <= options.drone_max_speed * options.cycle_length:
         return chosen_location - drone_location
     else:
-        return ((chosen_location - drone_location) / distance) * options.drone_max_speed * cycle_length
+        return ((chosen_location - drone_location) / distance) * options.drone_max_speed * options.cycle_length
 
 options = Namespace(
-    sensing_locations = [],
+    cycle_length = 1.0,
+    sensing_locations = np.array([]),
+    aois = np.array([]),
+    sensing_data_amounts = np.array([]),
+    drones_locations = np.array([]),
     sensing_locations_amount = 10,
+    cycle_stages = np.array([]),
+    data_transmission_cycle = 0.0,
     grid_size = 100.0,
     drones_amount = 5,
     drone_max_speed = 5.0,
     drone_bandwidth = 0.5,
-    total_location_data = 1.5
+    total_location_data = 1.5,
+    cycles_num = 1000
 )
 
 def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_locations_amount, drones_amount=options.drones_amount,
-            drone_max_speed=options.drone_max_speed, drone_bandwidth=options.drone_bandwidth, total_location_data=options.total_location_data):
+            drone_max_speed=options.drone_max_speed, drone_bandwidth=options.drone_bandwidth, total_location_data=options.total_location_data,
+            cycles_num=options.cycles_num):
     sensing_locations = (np.random.rand(sensing_locations_amount, 2) * grid_size) - (grid_size / 2)
     options.sensing_locations = sensing_locations
     options.sensing_locations_amount = sensing_locations_amount
@@ -192,16 +202,16 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
     options.drones_amount = drones_amount
     options.drone_max_speed = drone_max_speed
     options.drone_bandwidth = drone_bandwidth
+    options.cycles_num = cycles_num
 
     # -- BEGIN of the jupyter notebook's code (please refer to it for a more readable version)
 
-    cycle_length = 1.0
-    drones_locations = np.zeros((options.drones_amount, 2), dtype=float)
-    sensing_data_amounts = np.zeros(options.drones_amount, dtype=float)
-    aois = np.zeros(options.sensing_locations_amount, dtype=int)
-    chosen_locations = np.zeros(options.drones_amount, dtype=int)
-    cycle_stages = np.zeros(options.drones_amount, dtype=int)
-    data_transmission_cycle = options.drone_bandwidth * cycle_length
+    options.drones_locations = np.zeros((options.drones_amount, 2), dtype=float)
+    options.sensing_data_amounts = np.zeros(options.drones_amount, dtype=float)
+    options.aois = np.zeros(options.sensing_locations_amount, dtype=int)
+    options.chosen_locations = np.zeros(options.drones_amount, dtype=int)
+    options.cycle_stages = np.zeros(options.drones_amount, dtype=int)
+    options.data_transmission_cycle = options.drone_bandwidth * options.cycle_length
 
     class DurpEnv(py_environment.PyEnvironment):
         def __init__(self, drone):
@@ -228,13 +238,13 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
         def _step(self, action):
             chosen_location_index = int(action)
             accumulated_aoi = get_accumulated_aoi(self.cycle)
-            aois[chosen_location_index] = self.cycle
+            options.aois[chosen_location_index] = self.cycle
             new_accumulated_aoi = get_accumulated_aoi(self.cycle)
             aoi_multiplier = 0.05
             normalized_diff_aoi_component = \
                 ((1/(1+np.exp(- (accumulated_aoi - new_accumulated_aoi) * aoi_multiplier))) - 0.5) * 2.0
-            chosen_location = sensing_locations[chosen_location_index]
-            drone_location = drones_locations[self._drone]
+            chosen_location = options.sensing_locations[chosen_location_index]
+            drone_location = options.drones_locations[self._drone]
             distance = np.linalg.norm(chosen_location - drone_location)
             distance_multiplier = 0.03
             normalized_location_distance = (1 - (1/(1+np.exp(-distance * distance_multiplier)))) * 2.0
@@ -278,21 +288,21 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
         agent.initialize()
         agents.append(agent)
         environments.append(train_env)
-    num_iterations = 500
+    num_iterations = 20
     initial_collect_steps = 100
     collect_steps_per_iteration = 100
     batch_size = 64
 
     def collect_step(environment, policy, buffer):
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
-    next_time_step = environment.step(action_step.action)
-    traj = trajectory.from_transition(time_step, action_step, next_time_step)
-    buffer.add_batch(traj)
+        time_step = environment.current_time_step()
+        action_step = policy.action(time_step)
+        next_time_step = environment.step(action_step.action)
+        traj = trajectory.from_transition(time_step, action_step, next_time_step)
+        buffer.add_batch(traj)
 
     def collect_data(env, policy, buffer, steps):
-    for _ in range(steps):
-        collect_step(env, policy, buffer)
+        for _ in range(steps):
+            collect_step(env, policy, buffer)
 
     for i in range(len(agents)):
         agent = agents[i]
@@ -301,9 +311,9 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
             data_spec=agent.collect_data_spec,
             batch_size=env.batch_size)
         random_policy = random_tf_policy.RandomTFPolicy(env.time_step_spec(), env.action_spec())              
-        aois = np.zeros(options.sensing_locations_amount, dtype=int)
+        options.aois = np.zeros(options.sensing_locations_amount, dtype=int)
         collect_data(env, random_policy, replay_buffer, initial_collect_steps)
-        aois = np.zeros(options.sensing_locations_amount, dtype=int)
+        options.aois = np.zeros(options.sensing_locations_amount, dtype=int)
         dataset = replay_buffer.as_dataset(
         num_parallel_calls=3, 
         sample_batch_size=batch_size, 
@@ -312,7 +322,7 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
         iterator = iter(dataset)
         agent.train_step_counter.assign(0)
         for i in range(num_iterations):
-            aois = np.zeros(options.sensing_locations_amount, dtype=int)
+            options.aois = np.zeros(options.sensing_locations_amount, dtype=int)
             collect_data(env, agent.collect_policy, replay_buffer, 1)
             experience, unused_info = next(iterator)
             train_loss = agent.train(experience).loss
@@ -321,8 +331,8 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
     time_steps = []
     for drone in range(options.drones_amount):
         time_steps.append(environments[drone].reset())
-    cycles_num = 1000
-    aois = np.zeros(options.sensing_locations_amount, dtype=int)
+    
+    options.aois = np.zeros(options.sensing_locations_amount, dtype=int)
 
     # -- END of the jupyter notebook's code
 
@@ -333,34 +343,34 @@ def main(grid_size=options.grid_size, sensing_locations_amount=options.sensing_l
     simulator = Simulator(screen_w, screen_h)
     simulator.show()
 
-    for cycle in range(1, cycles_num + 1):
+    for cycle in range(1, options.cycles_num + 1):
         if cycle % 100 == 1:
             print(get_accumulated_aoi(cycle))
-            print(aois)
+            print(options.aois)
             print("----------------")
         for drone in range(options.drones_amount):
-            if cycle_stages[drone] == 0:
+            if options.cycle_stages[drone] == 0:
                 agent = agents[drone]
                 env = environments[drone]
                 agent.cycle = cycle
                 policy_step = agent.policy.action(time_steps[drone]) 
                 new_step = env.step(policy_step.action)
                 time_steps[drone] = new_step
-                chosen_locations[drone] = int(policy_step.action)
-                cycle_stages[drone] =  1
-            elif (drones_locations[drone] != sensing_locations[chosen_locations[drone]]).all():
+                options.chosen_locations[drone] = int(policy_step.action)
+                options.cycle_stages[drone] =  1
+            elif (options.drones_locations[drone] != options.sensing_locations[options.chosen_locations[drone]]).all():
                 traj = get_trajectory(drone)
-                new_location = drones_locations[drone] + traj
-                drones_locations[drone] = new_location
-            elif sensing_data_amounts[drone] == 0.0:
-                cycle_stages[drone] =  2
-                sensing_data_amounts[drone] = options.total_location_data
-                cycle_stages[drone] =  3
+                new_location = options.drones_locations[drone] + traj
+                options.drones_locations[drone] = new_location
+            elif options.sensing_data_amounts[drone] == 0.0:
+                options.cycle_stages[drone] =  2
+                options.sensing_data_amounts[drone] = options.total_location_data
+                options.cycle_stages[drone] =  3
             else:
-                sensing_data_amounts[drone] = np.max([sensing_data_amounts[drone] - data_transmission_cycle, 0.0])
-                if sensing_data_amounts[drone] == 0.0:
-                    aois[chosen_locations[drone]] = cycle
-                    cycle_stages[drone] =  0
+                options.sensing_data_amounts[drone] = np.max([options.sensing_data_amounts[drone] - options.data_transmission_cycle, 0.0])
+                if options.sensing_data_amounts[drone] == 0.0:
+                    options.aois[options.chosen_locations[drone]] = cycle
+                    options.cycle_stages[drone] =  0
         simulator._update()
         #time.sleep(1) # TODO: find a value that allows the user enjoy the simulation
     
